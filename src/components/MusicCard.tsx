bbb05181ade0225e9080fbf0acb3c53fb449e0d7
Vinicius Bueno Costa <buenov535@gmail.com>
2023-12-06 15:11:33 -0300

    [Requisito 5] - trybe tunes feito hehe :D

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SongType } from '../types';
+import checkedHeart from '../images/checked_heart.png';
+import emptyHeart from '../images/empty_heart.png';
 
-function MusicCard({ previewUrl, trackName }: Omit<SongType, 'trackId'>) {
+function MusicCard({ previewUrl, trackName, trackId }: SongType) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function handleFavorite() {
+    setIsFavorite(!isFavorite);
+  }
+
   return (
     <div>
       <h3>{trackName}</h3>
+      <label data-testid={ `checkbox-music-${trackId}` }>
+        <img src={ isFavorite ? checkedHeart : emptyHeart } alt="favorite" />
 
+        <input
+          onChange={ handleFavorite }
+          style={ { display: 'none' } }
+          checked={ isFavorite }
+          type="checkbox"
+        />
+      </label>
       <audio data-testid="audio-component" src={ previewUrl } controls>
         <track kind="captions" />
         O seu navegador não suporta o elemento
