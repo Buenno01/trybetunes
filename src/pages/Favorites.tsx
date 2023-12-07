28a8c3973bb6f85c942ce71b0f44a1b4f1b5fe71
Vinicius Bueno Costa <buenov535@gmail.com>
2023-12-07 11:07:41 -0300

    added loading screen to favaorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import { SongType } from '../types';
 import MusicCard from '../components/MusicCard';
+import Loading from '../components/Loading';
 
 function Favorites() {
   const [favorites, setFavorites] = useState<SongType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function fetchFavSongs() {
       const favSongs = await getFavoriteSongs();
       if (favSongs) {
         setFavorites(favSongs);
       }
+      setIsLoading(false);
     }
     fetchFavSongs();
   }, []);
+  if (isLoading) return <Loading />;
   return (
     <div>
-      <h2 className="text-xl text-center mb-4">Your favorite songs</h2>
+      <h2 className="text-xl text-center mb-4 font-bold">Your favorite songs</h2>
       <div className="flex flex-col gap-4 items-center">
         {
           favorites.map((song) => (
